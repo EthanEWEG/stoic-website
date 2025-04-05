59bf92542358233f9c7407b1b2c8f1a8abd6c3e9
EthanEWEG <133142961+EthanEWEG@users.noreply.github.com>
2025-04-05 09:27:10 -0400

    Delete website directory

--- website/scripts/page-transitions.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    document.body.classList.add('loaded');
-    document.body.classList.remove('hidden');
-});
-
-window.addEventListener('beforeunload', () => {
-    document.body.classList.add('hidden');
-});
